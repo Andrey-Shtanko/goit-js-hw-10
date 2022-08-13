3823e935e829496dd4ac22cbc7fa425ec497265a
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-08-13 16:43:04 +0300

    add-css

--- src/markUpForOneCountry.js
+++ src/markUpForOneCountry.js
@@ -1,17 +1,17 @@
 export default function markUpForOneCountry ({ name: { official }, capital, population, flags: { svg }, languages }) {
     const langList = Object.values(languages)
-    return `<div class='country-card'><img src="${svg}" alt="flag" width="30" height="30" class="country-flag" /><span
+    return `<div class='country-card'><img src="${svg}" alt="flag" width="40" height="30" class="item-img" /><span
   class="country-name"
 >${official}</span></div>
 <ul class="country-list">
   <li class="country-item">
-    <p class="country-capital">Capital: ${capital}</p>
+    <p class="country-capital">Capital: <span class="info-text">${capital}</span></p>
   </li>
   <li class="country-item">
-    <p class="country-population">Population: ${population}</p>
+    <p class="country-population">Population: <span class="info-text">${population}</span></p>
   </li>
   <li class="country-item">
-    <p class="country-lang">Languages: ${langList}</p>
+    <p class="country-lang">Languages: <span class="info-text">${langList}</span></p>
   </li>
 </ul>`      
 }
