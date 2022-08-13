c6d66a6dbe7e087dcad2d2733452c01aee641440
Andrey-Shtanko <97941755+Andrey-Shtanko@users.noreply.github.com>
2022-08-13 16:17:00 +0300

    add-flag-bg

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const DEBOUNCE_DELAY = 300;
 const inputField = document.querySelector(`#search-box`)
 const countryList = document.querySelector(`.country-list`)
 const countryInfo = document.querySelector(`.country-info`)
-const body = document.querySelector(`body`)
+const background = document.querySelector(`.background`)
 
 
 inputField.addEventListener(`input`, debounce(onInputSearch, DEBOUNCE_DELAY))
@@ -28,14 +28,16 @@ function onInputSearch(e) {
         } else if (country.length >= 2 && country.length <= 10) {
             for (let i = 0; i < country.length; i += 1) {
                 countryList.insertAdjacentHTML(`beforeend`,markUpForSeveralCountries(country[i]))
-                
+                background.style.backgroundImage = ``
             }
             console.log(country);
         } else if (country.length === 1) {
-                countryInfo.innerHTML = markUpForOneCountry(country)
-                // body.style.backgroundImage = country.flags.png
+                countryInfo.innerHTML = markUpForOneCountry(country[0])
+                const flagImg = country[0].flags.png
+                background.style.backgroundImage = ` url(${flagImg})`
             console.log(country);
-        } else {
+            } else {
+                background.style.backgroundImage = ``
             countryList.innerHTML=``
             Notiflix.Notify.failure('Oops, there is no country with that name')
         }}
